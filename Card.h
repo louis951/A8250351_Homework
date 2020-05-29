1e2ead37109e1f9f6c714aca9a3ae5cd755bb5e9
莊秉育 <59250126+louis951@users.noreply.github.com>
2020-05-29 19:37:49 +0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -18,13 +18,15 @@ public:
 	void askCard12();////發前兩張牌
 	void point(); //結算金錢
 private:
-	int card[53];//牌有53張
-	int Suit[4] = { '\6','\3','\4','\5' };//花色
-	int value[53];//牌的數值,設定1-11(A可為11 JQK為10)
-	int money = 10000;//你有多少錢，設定10000元
+	int card[52];//牌有52張
+	char cardName[13];//知道當前牌是什麼 比如AJQK這種顯示出來
+	char Suit[4];//花色
+	int valueA[5];//牌的數值
+	int valueB[5];
+	int money;//你有多少錢，設定10000元
 	int stake;//賭注
-	int cardNumA[5] = { 0,0,0,0,0 };//莊家的手牌
-	int cardNumB[5] = { 0,0,0,0,0 };//玩家的手牌
+	int cardNumA[5];//莊家的手牌
+	int cardNumB[5];//玩家的手牌
 	bool way;//判斷輸贏
 };
 #endif
