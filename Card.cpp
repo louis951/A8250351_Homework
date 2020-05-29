35fa3ee2c6e9d4f4f6b568786d51afcc3cd0af44
莊秉育 <59250126+louis951@users.noreply.github.com>
2020-05-29 19:38:08 +0800

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,23 @@
+#ifndef Card_H
+#define Card_H
+#include<ctime>
+#include<string>
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+
+Card::Card() {
+	srand(time(NULL));
+	for (int i=0; i < 5; i++) {
+		cardNumA[i] = 0;
+		cardNumB[i] = 0;
+	}
+	money = 10000;
+	char Suit[4] = { '\6','\3','\4','\5' };
+	char cardName[13] = { 'A','2','3','4' ,'5' ,'6' ,'7' ,'8','9','10','J','Q','K' };
+	int valueA[5] = {};
+	int valueB[5] = {};
+	for (int i = 0; i < 52; i++)
+		card[i] = i % 13 + 1;//把牌變成只有1~13
+	askCard12();
+}
