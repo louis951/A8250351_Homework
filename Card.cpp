78999d8a9409a479451e9f5a0e80ae459af7d5ee
莊秉育 <59250126+louis951@users.noreply.github.com>
2020-05-31 11:22:47 +0800

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -110,3 +110,54 @@ void Card::Shuffle() {
 	printNowA();
 	printNowB();
 }
+void Card::input() {
+	cout << "請下注" << endl;
+	cin >> stake;
+	for (; stake <= 0 || stake > money;) {
+		cout << "你下注金錢錯誤 請勿超過持有錢錢 請勿下注負數" << endl;
+		cin >> stake;
+	}
+	this->stake = stake;
+}
+int Card::judgeSizeA() {
+	int sum = 0;
+	int count = 0;
+	for (int i = 0; i < valueA; i++) {
+		sum += cardValue[cardNumA[i]];
+		if (cardValue[cardNumA[i]] == 1) {
+			count++;
+		}
+	}
+	if (sum <= 11 && count > 0) {
+		sum += 10;
+	}
+	return sum;
+}
+int Card::judgeSizeB() {
+	int sum = 0;
+	int count = 0;
+	for (int i = 0; i < valueB; i++) {
+		sum += cardValue[cardNumB[i]];
+		if (cardValue[cardNumB[i]] == 1) {
+			count++;
+		}
+	}
+	if (sum <= 11 && count > 0) {
+		sum += 10;
+	}
+	return sum;
+}
+void Card::point() {
+	if (money <= 0) {
+		cout << "你沒錢了,請問是否要賣屁股給資工1B吳佳凱獲得更多金錢？" << endl;
+		exit(0);
+	}
+	else
+		cout << money << endl;
+}
+int Card::getValueA() {
+	return valueA;
+}
+int Card::getValueB() {
+	return valueB;
+}
