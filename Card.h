bc921d4a8eebcf5e8fd4117d5700c6ad3b36b12d
莊秉育 <59250126+louis951@users.noreply.github.com>
2020-06-15 15:05:40 +0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -9,34 +9,35 @@
 #include <ctime>    //時間相關函數 
 using namespace std;
 
+template <typename T>
 class Card : public Bet{
 public:
 	Card();//
 	void input();//投入賭注
 	void Shuffle();//洗牌
-	int judgeSizeA()const;//判斷牌的大小是否超過21點
-	int judgeSizeB()const;
+	T judgeSizeA();//判斷牌的大小是否超過21點
+	T judgeSizeB();
 	void askCardB();//玩家要牌
 	void askCardA();//莊家小於17點一定要加牌
 	void askCard12();//發前兩張牌
-	void printNowA()const;//印出當前牌數 莊家
-	void printNowB()const;//印出當前牌數 玩家
+	void printNowA();//印出當前牌數 莊家
+	void printNowB();//印出當前牌數 玩家
 	void point()const; //結算金錢
 	void end();//最後結算
-	int getValueA()const;
-	int getValueB()const;
+	T getValueA();
+	T getValueB();
 	static int getRound();//第幾局
 private:
-	vector<int> card;//牌有52張
-	vector<int> cardValue;//牌的數值
+	vector<T> card;//牌有52張
+	vector<T> cardValue;//牌的數值
 	string cardName[53];;//牌的名子
-	vector<int> cardrandom;//亂數數值
-	vector<int> SuitrandomA;//數值花色
-	vector<int> SuitrandomB;
-	int valueA;//莊家牌數
-	int valueB;
-	vector<int> cardNumA;//莊家的手牌
-	vector<int> cardNumB;//玩家的手牌
+	vector<T> cardrandom;//亂數數值
+	vector<T> SuitrandomA;//數值花色
+	vector<T> SuitrandomB;
+	T valueA;//莊家牌數
+	T valueB;
+	vector<T> cardNumA;//莊家的手牌
+	vector<T> cardNumB;//玩家的手牌
 	static int round;
 };
 #endif
